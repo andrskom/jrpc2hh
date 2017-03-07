865bdffb8f8bd9d667f63c5dadf9f4af30cd2448
Andrey Skomorokhov <andrskom@MacBook-Pro-Andrey.local>
2017-03-07 03:56:44 +0300

    Some beautify code in main and start rewrite from doc to ast.

---
+++ testservice/testservice1.go
@@ -0,0 +1,40 @@
+package testservice
+
+import (
+	jModels "github.com/andrskom/jrpc2hh/models"
+	"models"
+	"net"
+)
+
+// jrpc2hh:service
+type Test1 struct {
+	db string // Test data
+}
+
+func NewTest1(db string) *Test1 {
+	return &Test1{db: db}
+}
+
+type Test1NilArgsResult struct {
+	SomeData string `json:"some_data"`
+}
+
+// jrpc2hh:method
+func (s *Test1) NilArgs(args jModels.NilArgs, res *Test1NilArgsResult) error {
+	return nil
+}
+
+type Test1NilResultArgs struct {
+	RequiredParam string `json:"required_param"`
+	OptionalParam *int   `json:"optional_param"`
+}
+
+// jrpc2hh:method
+func (s *Test1) NilResult(args Test1NilResultArgs, res *jModels.NilResult) error {
+	return nil
+}
+
+// jrpc2hh:method
+func (s *Test1) AnotherPackageResult(args jModels.NilArgs, res *models.SomeModel) error {
+	return nil
+}
